ec2a87c78e9211bd5582ff48e42beafc5950ae7d
drackenov <robertdraketalley@gmail.com>
2013-11-16 19:28:54 +0000

    cleanup

--- international_trade.rb
+++ international_trade.rb
@@ -6,7 +6,7 @@ require 'ostruct'
 include REXML
 require 'csv'
 
-rates_xml = "RATES.xml"
+rates_xml = "data/RATES.xml"
 rates_xmlfile = File.new(rates_xml)
 rates_doc = Document.new(rates_xmlfile)
 
@@ -14,7 +14,7 @@ rates = Rate_Converter.new(rates_doc)
 rates.find_rates
 rates_to_USD = rates.to_USD
 
-transactions = CSV.open "tran.csv"
+transactions = CSV.open "data/transactions.csv"
 trans_totals = Sales.new(transactions)
 puts trans_totals.compute_sum(rates_to_USD)
 
